cd1f5991ca2f9cb00e5b3600ffb087a8750da4bf
Woochang1132 <show4031132@gmail.com>
2024-01-05 23:06:02 +0900

    typeScript-repeat => LIFO, 맞게 구현되었는지 확인하기

--- src/4-oop-project/stack.ts
+++ src/4-oop-project/stack.ts
@@ -7,3 +7,54 @@ interface Stack<T> {
     push(value : T): void;
     pop(): T;
 }
+
+
+type gStackNode<T> = {
+    value : T,
+    next ?: gStackNode<T>
+}
+
+class gStackImp<T> implements Stack<T>{
+    _size: number = 0;
+    private head ?: gStackNode<T>
+    
+    get size(){
+        return this._size;
+    }
+
+    push(value: T): void {
+        const node : gStackNode<T> = {
+            value : value,
+            next : this.head
+        }
+        this.head = node;
+        this._size++; 
+    }
+
+    pop(): T {
+        if(this.head == null){
+            throw new Error('head state is null');
+        }
+        const node = this.head;
+        this.head = node.next; // node.next도 결국엔 StackNode를 담고 있다.
+
+        this._size--; 
+        return node.value;
+    }
+}
+
+const gstackResult = new gStackImp<string>()
+
+gstackResult.push('최우창');
+gstackResult.push('최우진');
+gstackResult.push('최우영');
+gstackResult.pop();
+
+const gstack = new gStackImp<number>()
+gstack.push(123);
+gstack.push(456);
+gstack.push(789);
+
+while(gstack.size !==0 ){
+    console.log(gstack.pop())
+}
